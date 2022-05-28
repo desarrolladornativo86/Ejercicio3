ebfd6cd38000d4940bbd7d53c38f61567a405c6b
desarrolladornativo86 <106443583+desarrolladornativo86@users.noreply.github.com>
2022-05-28 18:50:55 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(10, 30, 50);
+        System.out.println("Resultado: " + resultado);
+        //Resolvemos coche
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuerta();
+        System.out.println("El total de puertas ahora son: " +miCoche.puertas);
+            }
+            public static int suma(int a, int b, int c){
+                return a + b + c;
+            }
+        }
